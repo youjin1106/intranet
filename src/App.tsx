2bcefc4d79a616feda927015537b066b992d21eb
dmswnlee <dlrmawnl03@gmail.com>
2024-01-30 13:43:56 +0900

    추가 : 로그인페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 import Mypage from "./pages/Mypage";
 import Notice from "./pages/Notice";
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -15,10 +16,14 @@ function App() {
         <li>
           <Link to="/notice">공지사항</Link>
         </li>
+        <li>
+          <Link to="/login">로그인</Link>
+        </li>
       </ul>
       <Routes>
         <Route path="/" element={<Mypage />} />
         <Route path="/notice" element={<Notice />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
