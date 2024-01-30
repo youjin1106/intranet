5b1efa90b26b757c8dd2a8dcfbf9033ce533889c
ssmv713 <sshim9619@naver.com>
2024-01-30 13:35:10 +0900

    수정: 충돌 해결

--- src/pages/Notice.tsx
+++ src/pages/Notice.tsx
@@ -1,14 +1,8 @@
 const Notice = () => {
   return (
-    <>
-      <h1 className="text-3xl text-primary font-bold underline decoration-border">
-        테스트 해보는중
-      </h1>
-      <p className="text-s">s</p>
-      <p className="text-lg">lg</p>
-      <p className="text-titleMd">titleMd</p>
-      <div className="rounded w-[100px] h-[100px] bg-primary">테스트 박스</div>
-    </>
+    <div className="layout main-display">
+      <h1>공지</h1>
+    </div>
   );
 };
 export default Notice;
