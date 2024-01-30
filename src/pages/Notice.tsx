62976eaf000234133a80343b99aea239e9b63327
ssmv713 <sshim9619@naver.com>
2024-01-30 11:11:44 +0900

    추가: 스타일 테마 적용

--- src/pages/Notice.tsx
+++ src/pages/Notice.tsx
@@ -1,4 +1,14 @@
 const Notice = () => {
-  return <h1 className="text-3xl font-bold underline">비트테스트 해보는중</h1>;
+  return (
+    <>
+      <h1 className="text-3xl text-primary font-bold underline decoration-border">
+        비트테스트 해보는중
+      </h1>
+      <p className="text-s">s</p>
+      <p className="text-lg">lg</p>
+      <p className="text-titleMd">titleMd</p>
+      <div className="rounded w-[100px] h-[100px] bg-primary">테스트 박스</div>
+    </>
+  );
 };
 export default Notice;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontSize: {
+      s: ["14px", "16px"],
+      md: ["16px", "24px"],
+      mdBold: ["16px", "24px"],
+      xBold: ["40px", "48px"],
+      lg: ["20px", "24px"],
+      titleLg: ["32px", "40px"],
+      titleMd: ["24px", "32px"],
+    },
+    colors: {
+      bg00: "#F0F2F5",
+      bg01: "#fff",
+      gray00: "#2E2F30",
+      gray01: "#6E6F70",
+      primary: "#1C92FF",
+      primaryDark: "#0A78DE",
+      border: "rgba(0, 0, 0, 0.05)",
+      white: "#fff",
+    },
+    borderRadius: {
+      DEFAULT: "16px",
+      button: "24px",
+    },
   },
   plugins: [],
 };
