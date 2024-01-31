8c4690daa8fcca9b237600e4ff910d31b6ef44d7
dmswnlee <dlrmawnl03@gmail.com>
2024-01-31 12:46:39 +0900

    추가 : 로그인 폼 스타일 설정

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,8 +1,18 @@
-export default function LoginForm() {
-  return (
-    <div>
-      LoginForm
-    </div>
-  );
-}
+import logo from '../../assets/logo.svg';
+import PrimaryButton from '../buttons/PrimaryButton';
+import InputBox from './InputBox';
 
+export default function LoginForm() {
+   return (
+      <div className="w-[484px] h-[432px] bg-white p-10 rounded flex flex-col justify-around gap-[64px]">
+         <h1 className="logo">
+            <img src={logo} alt="logo" />
+         </h1>
+         <div className='flex flex-col gap-[16px]'>
+            <InputBox label={'아이디'}/>
+            <InputBox label={'비밀번호'}/>
+         </div>
+        <PrimaryButton label={'로그인'} size={'lg'} />
+      </div>
+   );
+}
