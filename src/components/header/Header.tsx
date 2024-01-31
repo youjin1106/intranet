ef1c75c86827abf6b237b24fec7d8512ee3c226d
dmswnlee <dlrmawnl03@gmail.com>
2024-01-31 12:21:05 +0900

    추가 : header 스타일 설정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,16 @@
-export default function Header() {
+type TitleProps = {
+  label:string
+}
+
+export default function Header({label} : TitleProps) {
+
   return (
-    <header>
-      Header
+    <header className='p-[16px] flex justify-between'>
+      <h1 className='text-titleLg font-bold text-gray00'>{label}</h1>
+      <div>
+        <p className='text-lg font-medium'>오후 3:00:00</p>
+        <span className='text-s text-gray01'>2024년 1월 25일 목요일</span>
+      </div>
     </header>
   );
 }
