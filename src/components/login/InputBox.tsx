fb10450e2b95fb6b0f9c73b8ac53a251861526f3
dmswnlee <dlrmawnl03@gmail.com>
2024-02-01 12:42:46 +0900

    추가 : props 속성

--- src/components/login/InputBox.tsx
+++ src/components/login/InputBox.tsx
@@ -1,15 +1,16 @@
 type PlaceholderProps = {
-  label:string
-}
+   type: string;
+   label: string;
+};
 
-export default function InputBox({label}: PlaceholderProps) {
+export default function InputBox({ label, type }: PlaceholderProps) {
    return (
       <div>
-         <input 
-            type="text" 
+         <input
+            type={type}
             placeholder={label}
-            className="w-full h-[40px] rounded-button py-[10px] px-[16px] outline-none border border-solid border-gray01" 
-          />
+            className="w-full h-[40px] rounded-button py-[10px] px-[16px] outline-none border border-solid border-gray01"
+         />
       </div>
    );
 }
