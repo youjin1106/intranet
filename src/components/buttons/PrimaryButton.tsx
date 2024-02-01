da9baeca4161c53d21b6b5a1358da0dfa5d66830
JeonYooDeok <jyd910215@gmail.com>
2024-02-02 01:49:32 +0900

    버튼 온클릭 프롭스 추가

--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -1,19 +1,33 @@
 type PrimaryButtonProps = {
-  label:string
-  size:string
-}
+  label: string;
+  size: string;
+  onClick?: () => void;
+};
 
-const PrimaryButton:React.FC<PrimaryButtonProps> = ({label, size}) => {
-  if(size === "md"){
+const PrimaryButton: React.FC<PrimaryButtonProps> = ({
+  label,
+  size,
+  onClick,
+}) => {
+  if (size === "md") {
     return (
-      <button className="text-white text-mdBold px-4 py-1 bg-primary hover:bg-primaryDark rounded inline-flex">{label}</button>
-    )
-  }else if(size === "lg"){
+      <button
+        className="text-white text-mdBold px-4 py-1 bg-primary hover:bg-primaryDark rounded inline-flex"
+        onClick={onClick}
+      >
+        {label}
+      </button>
+    );
+  } else if (size === "lg") {
     return (
-      <button className="w-full text-white text-mdBold px-4 py-3 bg-primary hover:bg-primaryDark rounded-button inline-flex justify-center">{label}</button>
-    )
+      <button
+        className="w-full text-white text-mdBold px-4 py-3 bg-primary hover:bg-primaryDark rounded-button inline-flex justify-center"
+        onClick={onClick}
+      >
+        {label}
+      </button>
+    );
   }
-  
-}
+};
 
-export default PrimaryButton
+export default PrimaryButton;
