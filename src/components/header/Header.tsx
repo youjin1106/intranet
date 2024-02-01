5552f863167049d920eba7b1904da9b08b08384a
dmswnlee <dlrmawnl03@gmail.com>
2024-02-01 11:17:21 +0900

    추가 : header 타이머 기능

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,35 @@
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+dayjs.locale('ko');
+
 type TitleProps = {
-  label:string
+   label: string;
+};
+
+export default function Header({ label }: TitleProps) {
+   const [clock, setClock] = useState('');
+   const [date, setDate] = useState('');
+
+   useEffect(() => {
+      const Timer = setInterval(() => {
+         const now = dayjs();
+         setClock(`${now.format('A HH:mm:ss')}`);
+         setDate(`${now.format('YYYY년 MM월 DD일 ddd요일')}`);
+      }, 1000);
+
+      return () => {
+         clearInterval(Timer);
+      };
+   }, []);
+
+   return (
+      <header className="p-[16px] flex justify-between items-center">
+         <h1 className="text-titleLg font-bold text-gray00">{label}</h1>
+         <div className="flex flex-col items-end">
+            <p className="text-lg font-medium">{clock}</p>
+            <span className="text-s text-gray01">{date}</span>
+         </div>
+      </header>
+   );
 }
-
-export default function Header({label} : TitleProps) {
-
-  return (
-    <header className='p-[16px] flex justify-between'>
-      <h1 className='text-titleLg font-bold text-gray00'>{label}</h1>
-      <div>
-        <p className='text-lg font-medium'>오후 3:00:00</p>
-        <span className='text-s text-gray01'>2024년 1월 25일 목요일</span>
-      </div>
-    </header>
-  );
-}
-
