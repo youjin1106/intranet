f5cd365e3c4fe811120f7ac3070e3a5c5d223837
dmswnlee <dlrmawnl03@gmail.com>
2024-02-03 18:58:06 +0900

    추가 : 마이페이지 공지사항 슬라이드 기능

--- src/components/mypage/notice-slide/NoticeSlide.tsx
+++ src/components/mypage/notice-slide/NoticeSlide.tsx
@@ -1,42 +1,46 @@
-import { useEffect, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
+type Notice = {
+	title: string;
+};
 
 export default function NoticeSlide() {
-   const [notice, setNotice] = useState([]);
+	const [notice, setNotice] = useState<string[]>([]);
 
-   useEffect(() => {
-      fetch('http://localhost:3001/header-notice', {
-         headers: {
-            Accept: 'application/json',
-         },
-      })
-         .then(res => res.json())
-         .then(data => {
-            const titlesArray = data.map(item => item.titles);
-            setNotice(titlesArray);
-         })
-         .catch(err => {
-            console.error(`Error : ${err}`);
-         });
-   }, []);
+	useEffect(() => {
+		fetch("http://localhost:3001/header-notice", {
+			headers: {
+				Accept: "application/json",
+			},
+		})
+			.then(res => res.json())
+			.then((data: Notice[]) => {
+				const titlesArray = data.map(item => item.title);
+				setNotice(titlesArray);
+			})
+			.catch(err => {
+				console.error(`Error : ${err}`);
+			});
+	}, []);
 
-   return (
-      <div className="h-[56px] p-[16px] flex gap-[8px] text-gray00">
-         <span className="material-symbols-outlined">notifications_active</span>
-            <Swiper
-               className='overflow-hidden'
-               direction={'vertical'}
-               autoplay={{
-                  delay: 3500,
-                  disableOnInteraction: false,
-               }}
-               loop={true}
-               modules={[Autoplay]}>
-               {notice.map((item, index) => (
-                  <SwiperSlide key={index}>{item}</SwiperSlide>
-               ))}
-            </Swiper>
-      </div>
-   );
+	return (
+		<div className="h-[56px] p-[16px] flex gap-[8px] text-gray00">
+			<span className="material-symbols-outlined">notifications_active</span>
+			<Swiper
+				className="overflow-hidden"
+				direction={"vertical"}
+				autoplay={{
+					delay: 3500,
+					disableOnInteraction: false,
+				}}
+				loop={true}
+				modules={[Autoplay]}>
+				{notice.map((item, index) => (
+					<SwiperSlide key={index}>{item}</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
+	);
 }
