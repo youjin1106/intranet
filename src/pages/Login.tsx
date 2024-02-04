6fa83c44ef21d003d37ae85a2442b3475900e3ec
dmswnlee <dlrmawnl03@gmail.com>
2024-02-05 00:25:26 +0900

    추가 : 로그인페이지 스타일, 기능

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,42 @@
-import LoginForm from '../components/login/LoginForm';
+import LoginForm from "../components/login/LoginForm";
+import { useAtom } from "jotai";
+import { useNavigate } from "react-router-dom";
+import { isLoggedInAtom } from "../App";
+
+type User = {
+	email: string;
+	password: string;
+};
 
 export default function Login() {
-  return (
-    <div className='w-screen bg-primary flex justify-center items-center'>
-      <LoginForm />
-    </div>
-  );
-}
+	const [isLoggedIn, setIsLoggedIn] = useAtom(isLoggedInAtom);
+	const navigate = useNavigate();
 
+	const handleLogin = async (email: string, password: string) => {
+		await fetch("http://localhost:3001/user", {
+			headers: {
+				Accept: "application/json",
+			},
+		})
+			.then(res => res.json())
+			.then((data: User[]) =>
+				data.map(user => {
+					if (user.email === email && user.password === password) {
+						sessionStorage.setItem("isLoggedIn", JSON.stringify(true));
+						sessionStorage.setItem("loggedInUserEmail", user.email);
+						setIsLoggedIn(true);
+						navigate("/");
+					}
+				}),
+			)
+			.catch(err => {
+				console.error(`Error : ${err}`);
+			});
+	};
+
+	return (
+		<div className="w-screen h-screen bg-primary flex justify-center items-center">
+			<LoginForm onLogin={handleLogin} />
+		</div>
+	);
+}
