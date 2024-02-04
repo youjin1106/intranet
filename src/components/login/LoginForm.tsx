9ab8f5e7403c20fbf1fbe8086bd75b59b7bc3974
dmswnlee <dlrmawnl03@gmail.com>
2024-02-05 00:26:22 +0900

    추가 : 로그인폼 스타일, 기능

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,18 +1,57 @@
-import logo from '../../assets/logo.svg';
-import PrimaryButton from '../buttons/PrimaryButton';
-import InputBox from './InputBox';
+import { useState } from "react";
+import logo from "../../assets/logo.svg";
+import InputBox from "./InputBox";
 
-export default function LoginForm() {
-   return (
-      <div className="w-[484px] h-[432px] bg-white p-10 rounded flex flex-col justify-around gap-[64px]">
-         <h1 className="logo">
-            <img src={logo} alt="logo" />
-         </h1>
-         <div className='flex flex-col gap-[16px]'>
-            <InputBox label={'아이디'}/>
-            <InputBox label={'비밀번호'}/>
-         </div>
-        <PrimaryButton label={'로그인'} size={'lg'} />
-      </div>
-   );
+interface LoginFormProps {
+	onLogin: (email: string, password: string) => void;
+}
+
+export default function LoginForm({ onLogin }: LoginFormProps) {
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		onLogin(formData.email, formData.password);
+	};
+
+	const onChangeFormData = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	return (
+		<form
+			className="w-[484px] h-[432px] bg-white p-10 rounded flex flex-col justify-around gap-[64px]"
+			onSubmit={handleSubmit}>
+			<h1 className="logo">
+				<img src={logo} alt="logo" />
+			</h1>
+			<div className="flex flex-col gap-[16px]">
+				<InputBox
+					type="text"
+					placeholder="이메일주소"
+					name="email"
+					value={formData.email}
+					onChange={onChangeFormData}
+				/>
+				<InputBox
+					type="password"
+					placeholder="비밀번호"
+					name="password"
+					value={formData.password}
+					onChange={onChangeFormData}
+				/>
+			</div>
+			<button
+				className="w-full text-white text-mdBold px-4 py-3 bg-primary hover:bg-primaryDark rounded-button inline-flex justify-center"
+				type="submit">
+				Login
+			</button>
+		</form>
+	);
 }
