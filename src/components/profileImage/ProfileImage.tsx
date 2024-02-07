ae2d921ecbab94e2d9a1c606308a13ae97fe3f9c
ssmv713 <sshim9619@naver.com>
2024-02-07 15:33:48 +0900

    수정: navbar 너비 조정

--- src/components/profileImage/ProfileImage.tsx
+++ src/components/profileImage/ProfileImage.tsx
@@ -1,21 +1,32 @@
 type ProfileImageProps = {
-	size?: string;
-	isOnline?: boolean;
-	src?: string | null | undefined;
+  size?: string;
+  isOnline?: boolean;
+  src?: string | null | undefined;
 };
 
 export const ProfileImage = ({ size, isOnline, src }: ProfileImageProps) => {
-	return (
-		<div className="relative w-fit">
-			<div className={` rounded-[999px] overflow-hidden aspect-square ${size === "lg" ? "w-[116px]" : "w-[50px]"}`}>
-				<img className="size-full object-cover" src={src || undefined} alt="profile" />
-			</div>
-			<div
-				className={`border border-solid border-white rounded-[999px] overflow-hidden aspect-square absolute  right-0 ${
-					size === "lg" ? "w-[24px] border-[4px] bottom-1.5" : "w-[14px] border-[2px] bottom-0"
-				} ${isOnline ? "bg-primary" : "bg-[#ccc]"}`}></div>
-		</div>
-	);
+  return (
+    <div className="relative w-fit">
+      <div
+        className={` rounded-[999px] overflow-hidden aspect-square ${
+          size === "lg" ? "w-[100px]" : "w-[50px]"
+        }`}
+      >
+        <img
+          className="size-full object-cover"
+          src={src || undefined}
+          alt="profile"
+        />
+      </div>
+      <div
+        className={`border border-solid border-white rounded-[999px] overflow-hidden aspect-square absolute  right-0 ${
+          size === "lg"
+            ? "w-[24px] border-[4px] bottom-1.5"
+            : "w-[14px] border-[2px] bottom-0"
+        } ${isOnline ? "bg-primary" : "bg-[#ccc]"}`}
+      ></div>
+    </div>
+  );
 };
 
 //
