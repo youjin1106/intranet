b6122352efa7dede0386a06ec16a5a7b438f070f
ssmv713 <sshim9619@naver.com>
2024-02-08 16:09:49 +0900

    수정: 공지사항수정

--- src/pages/Notice.tsx
+++ src/pages/Notice.tsx
@@ -57,11 +57,11 @@ const Notice = () => {
 
   return (
     <div className="h-lvh flex flex-col gap-[10px] bg-bg00 flex-1 p-[24px]">
-      <Header label="공지 사항" />
+      <Header label="공지사항" />
       <div className="flex-1 flex flex-col justify-between bg-bg01 p-[16px] rounded">
         <div>
           <div className="border-solid border-y border-border order-indigo-600 flex justify-between px-[12px] py-[8px]">
-            <span className="text-mdBold">제목</span>
+            <span className="text-mdBold">제목입니다</span>
             <span className="text-mdBold">등록일</span>
           </div>
 
